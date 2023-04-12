ee975d4a676863cfd1b5f7d3219ed0eb0b3df4ca
matt2ology <34324060+matt2ology@users.noreply.github.com>
2023-04-12 01:09:24 -0700

    Init commit

---
+++ assignment05-create-sql-triggers-functional-dependencies/drop_all.sql
@@ -0,0 +1,36 @@
+SHOW TABLES; -- this is to show the tables before dropping them
+
+/* Drop TRIGGERS */
+
+DROP TRIGGER IF EXISTS Inc_enrollment_number;
+DROP TRIGGER IF EXISTS Del_enrollment_number;
+
+/* Drop PROCEDUREs */
+
+DROP PROCEDURE IF EXISTS Get_TA;
+DROP PROCEDURE IF EXISTS GetStudentCourse;
+
+/* Drop FUNCTIONs */
+
+DROP FUNCTION IF EXISTS Course_Instructor;
+
+/* Drop VIEWs */
+
+DROP VIEW IF EXISTS TAView;
+DROP VIEW IF EXISTS OnlineCourseView;
+DROP VIEW IF EXISTS InPersonCourseView;
+DROP VIEW IF EXISTS Student_Grade_A;
+DROP VIEW IF EXISTS TA_Course;
+
+/* Drop TABLEs */
+
+DROP TABLE IF EXISTS Enrolled;
+DROP TABLE IF EXISTS Instructor;
+DROP TABLE IF EXISTS OnlineCourse;
+DROP TABLE IF EXISTS InPersonCourse;
+DROP TABLE IF EXISTS Course;
+DROP TABLE IF EXISTS TA;
+DROP TABLE IF EXISTS Student;
+DROP TABLE IF EXISTS Classroom;
+
+SHOW TABLES; -- this is to show the tables after dropping them
